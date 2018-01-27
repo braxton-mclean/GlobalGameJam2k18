7b3fbe663fc763e00bb667a07fec9a8fac2b6291
bcabio <briancabigon@gmail.com>
2018-01-27 05:30:36 -0500

    Added projectile shooting and firerate mechanics

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,28 +3,54 @@ extends Node2D
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
-var PLAYER_SPEED = 10
-var player_node
+export var PLAYER_SPEED = 10
+export var BULLET_OFFSET = 30
+export var BULLET_SPEED = 10
+var bullet
+
+# Used to calculate fire rate
+var delta_sum = 0
 
 func _ready():	
+	bullet = preload('res://scenes/scene_projectile.tscn')
 	set_process(true)
 
 func _process(delta):
+	var mouse_pos = get_viewport().get_mouse_pos()
+	var player_pos = self.get_pos()
+
+	
+	delta_sum = delta_sum + delta
+	
+	_move()
+	
+	_shoot(player_pos, mouse_pos)
+#	get_node('Sprite').set_pos(player_pos)
+# - player_pos.x, mouse_y - player_pos.y)
+
+func _move():
 	var direction_vert = Vector2(0,0)
 	var direction_hor = Vector2(0,0)
 	
 	if Input.is_action_pressed("move_up"):
-		direction_vert = Vector2(0,-1)
+		direction_vert = Vector2(0, -1)
 	elif Input.is_action_pressed("move_down"):
-		direction_vert = Vector2(0,1)
-	
-	if Input.is_action_pressed("move_right"):
-		direction_hor = Vector2(1,0)
-	elif Input.is_action_pressed("move_left"):
-		direction_hor = Vector2(-1,0)
+		direction_vert = Vector2(0, 1)
 		
+	if Input.is_action_pressed("move_right"):
+		direction_hor = Vector2(1, 0)
+	elif Input.is_action_pressed("move_left"):
+		direction_hor = Vector2(-1, 0)
+	
 	var direction = (direction_vert + direction_hor).normalized()
-	self.translate(direction* PLAYER_SPEED)
-
-#	get_node('Sprite').set_pos(player_pos)
-# - player_pos.x, mouse_y - player_pos.y)
+	self.translate(direction * PLAYER_SPEED)
+	
+func _shoot(player_position, mouse_position):
+	if Input.is_action_pressed("left_click"):
+		if delta_sum > .5:
+			var projectile = bullet.instance()
+			var direction = mouse_position - player_position
+			projectile.set_pos(player_position + direction.normalized() * BULLET_OFFSET)
+			projectile._set_direction(direction.normalized() * BULLET_SPEED)
+			get_parent().add_child(projectile)
+			delta_sum = 0
--- scripts/script_projectile.gd
+++ scripts/script_projectile.gd
@@ -2,22 +2,22 @@ extends Area2D
 
 # class member variables go here, for example:
 
-export var speed = 10
+export var speed = 50
 onready var animation = get_node("AnimationPlayer")
+onready var bullet_sprite = get_node("AnimatedSprite")
 var direction = Vector2(1, 1) # FOR THE CHARACTER 
 export var deathAnimationName = "enter name"
 export var continuousAnimationName = "enter name"
 
 func _ready():
 	# Called every time the node is added to the scene.
-	# Initialization here
-	animation.play(continuousAnimationName)
+	# Initialization 
 	set_fixed_process(true)
 	pass
 	
 
 func _fixed_process(delta):
-	translate(direction*speed)
+	translate(direction*speed*delta)
 
 func _hit_something():
 	set_process(false)
@@ -25,12 +25,12 @@ func _hit_something():
 
 func _on_Projectile_area_enter( area ):
 	# Hit an enemy or asteroid
-	if (area.has_method("health")):#included in the enemy
+	if (area.has_method("takedamage")):#included in the enemy
 		# Duck typing at it's best
 		area.takedamage()
 		_hit_something()
 	print ("hit area2d")
-	get_node("AnimationPlayer").play(deathAnimationName)
+
 
  # replace with function body
 
@@ -40,15 +40,15 @@ func _on_Projectile_body_enter( body ):
 
 func _destroy():
 	#add animation
-	animation.play("INSERT_ANIM_NAME")
+
 	queue_free()
 	
 func _on_Timer_timeout():
 	queue_free()
-	_destroy()
 
-func setDirection(mousevector):
-	direction = mousevector
+
+func _set_direction(vector):
+	direction = vector
 
 func setSpeed( desiredVelocity ):
 	speed = desiredVelocity
