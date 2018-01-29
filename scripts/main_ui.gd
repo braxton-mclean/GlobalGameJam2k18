5ed5e1805345d4b4f928a0204ee506dc5b1be0a2
Braxton McLean <braxton.mclean5@gmail.com>
2018-01-29 09:36:23 -0500

    Added master scene talk back

--- scripts/main_ui.gd
+++ scripts/main_ui.gd
@@ -2,17 +2,6 @@ extends Control
 
 func _ready():
 	pass
-
-func _on_StartImage_pressed():
-	print('lmao')
-	pass # replace with function body
-
-
-func _on_StartImage_mouse_enter():
-	print("I see a mouse")
-	pass # replace with function body
-
-
-func _on_StartImage_button_down():
-	print("I have been pressed")
-	pass # replace with function body
+	
+func test(source):
+	print(str(source))
--- scripts/title_screen_ui.gd
+++ scripts/title_screen_ui.gd
@@ -14,13 +14,11 @@ func _ready():
 
 func _on_StartImage_pressed():
 	print("lel")
+	if(get_tree().get_root().has_node("master_scene_node/MasterSceneUI")):
+		var master_ui_node = get_tree().get_root().get_node("master_scene_node/MasterSceneUI")
+		master_ui_node.test("Start Button Press")
+	
 	pass # replace with function body
-
-
-func _on_StartImage_mouse_enter():
-	print("help")
-	pass # replace with function body
-
 
 func _on_StartImage_focus_enter():
 	print("got focus")
